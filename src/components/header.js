ba9e146d55d7ea5fb07b4d2a6676da58ef636823
brendamatias <brendamatias.sobral@gmail.com>
2020-04-09 22:23:49 -0300

    home page

--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,38 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "./header.css"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
+import logo from "../images/logo.svg"
+
+const Header = () => (
+  <header>
+    <nav>
+      <div>
+        <Link to="/">
+          <img src={logo} alt="Covid-19" />
         </Link>
-      </h1>
-    </div>
+      </div>
+
+      <ul>
+        <li>
+          <a href="#">Overview</a>
+        </li>
+        <li>
+          <a href="#">Contagion</a>
+        </li>
+        <li>
+          <a href="#">Preventions</a>
+        </li>
+        <li>
+          <a href="#">Symptoms</a>
+        </li>
+        <li>
+          <button>Contact Us</button>
+        </li>
+      </ul>
+    </nav>
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+
+import background from "../images/background.png"
+import gif from "../images/3609101.svg"
+
+import "./index.css"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div
+      className="home"
+      style={{
+        backgroundImage: `url(${background})`,
+      }}
+    >
+      <div className="container">
+        <div>
+          <h3>Let's Control</h3>
+          <h1>You are at home?</h1>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever.
+          </p>
+          <button>Read More</button>
+        </div>
+        <img src={gif} alt="Stay At Home" />
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
